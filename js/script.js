dcc4f22acc2b51e458a43cde0f1e090854a85448
unseenPrime <dimaturokt23@gmail.com>
2023-05-02 17:45:20 +0300

    Made a slider to the course block without adaptive.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Burger menu
 function toggleMenu() {
     document.querySelector('.menu').classList.toggle('active');
     document.querySelector('.burger').classList.toggle('active');
@@ -5,3 +6,37 @@ function toggleMenu() {
 
 // Attach an event handler to a click on the burger menu
 document.querySelector('.burger').addEventListener('click', toggleMenu);
+//Slider 1
+$(document).ready(function(){
+    var numOfItems = $('.course__item').length;
+    var showDots = (numOfItems > 3);
+
+    $('.course__content').slick({
+        arrows: false,
+        dots: showDots,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 850,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
